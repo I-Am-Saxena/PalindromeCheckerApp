e4668832024dcc33a34a28735055975c808b9ca0
Amol Saxena <amolsaxena.personal@gmail.com>
2026-02-26 15:32:52 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,5 +3,26 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Palindrome Checker Application");
         System.out.println("Version: 1.0.0");
+        System.out.println();
+
+        // UC2: Hardcoded string palindrome check
+        String word = "madam";  // Hardcoded string
+        boolean isPalindrome = true;
+
+        // Check palindrome
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is not a palindrome.");
+        }
     }
 }
